c8997843c9d8d17f2475404549c602d1f4b5064b
Krzysztof Majda <kmajda@outlook.com>
2021-05-17 22:50:53 +0200

    flaski sraski duparaskiasd

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import render_template, flash, redirect, url_for, request, session
 import numpy as np
-
+import message
 
 app = Flask(__name__)
 
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/strona_startowa')
 def strona_startowa():
-    return render_template('strona_startowa.html')
+    return render_template('strona_startowa.html', info=message.main_page(), loged_in=loged_in)
 
 
 if __name__ == '__main__':
