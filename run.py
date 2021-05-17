ca49f65fe958c37dc7b75711b8dbdae24e54e658
Krzysztof Majda <kmajda@outlook.com>
2021-05-17 22:36:11 +0200

    templatki i fotki i cssy

--- run.py
+++ run.py
@@ -4,8 +4,14 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/')
+@app.route('/strona_startowa')
+def strona_startowa():
+    if 'log_in_user_id' in session or 'activate_user_id' in session:
+        loged_in = True
+    else:
+        loged_in = False
+    return render_template('strona_startowa.html', info=message.main_page(), loged_in=loged_in)
 
 
 if __name__ == '__main__':
