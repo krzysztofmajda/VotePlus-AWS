43b1ff90330e6f95062e39291dace461a8c3ec27
Krzysztof Majda <kmajda@outlook.com>
2021-05-17 22:46:31 +0200

    flaski sraski duparaski

--- run.py
+++ run.py
@@ -10,11 +10,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/strona_startowa')
 def strona_startowa():
-    if 'log_in_user_id' in session or 'activate_user_id' in session:
-        loged_in = True
-    else:
-        loged_in = False
-    return render_template('strona_startowa.html', info=message.main_page(), loged_in=loged_in)
+    return render_template('strona_startowa.html')
 
 
 if __name__ == '__main__':
