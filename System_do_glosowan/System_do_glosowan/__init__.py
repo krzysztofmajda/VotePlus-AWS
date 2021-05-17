468fe3ba521986920b78cf0a89aa1285496d118d
Krzysztof Majda <kmajda@outlook.com>
2021-05-17 21:55:28 +0200

    dupaaaaaaaaa tell me whyyyyyyyyyyyyyyyyyyyyyyy

--- System_do_glosowan/System_do_glosowan/__init__.py
+++ System_do_glosowan/System_do_glosowan/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
 
-from System_do_glosowan import views
-from System_do_glosowan import fun_base
-from System_do_glosowan import fun
-from System_do_glosowan import fun_mail
-from System_do_glosowan import message
+import views
+import fun_base
+import fun
+import fun_mail
+import message
 
 app.run(debug=True)
