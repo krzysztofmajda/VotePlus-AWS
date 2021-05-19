962484e50afa595fd03ac795a59593ce4ea40343
Krzysztof Majda <kmajda@outlook.com>
2021-05-19 16:59:38 +0200

    Initial commit

---
+++ System_do_glosowan/__init__.py
@@ -0,0 +1,14 @@
+"""
+The flask application package.
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+import System_do_glosowan.views
+import System_do_glosowan.fun_base
+import System_do_glosowan.fun
+import System_do_glosowan.fun_mail
+import System_do_glosowan.message
+
+app.secret_key = fun.random_string(50)
