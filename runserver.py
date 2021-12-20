ef1b1441e88d8ebcd0b490fd94956bfa5ad57108
krzysztofmajda <kmajda@outlook.com>
2021-12-20 11:52:34 +0100

    port change

--- runserver.py
+++ runserver.py
@@ -4,7 +4,7 @@ from System_do_glosowan import app
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', '0.0.0.0')
     try:
-        PORT = int(environ.get('SERVER_PORT', '8080'))
+        PORT = int(environ.get('SERVER_PORT', '8000'))
     except ValueError:
-        PORT = 8080
+        PORT = 8000
     app.run(HOST, PORT)
